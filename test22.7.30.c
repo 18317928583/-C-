497240fb415bf65acb4dc10406b11e3e5df49133
Dawn <110223824+18317928583@users.noreply.github.com>
2022-08-01 15:13:11 +0800

    Add files via upload

---
+++ test22.7.30.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#include<string.h>
+int main()
+{
+	//int arr[] = "hello";
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c' };
+	printf("%d\n", strlen(arr1));
+	printf("%d\n", strlen(arr2));
+
+	
+	
+	return 0;
+
+}
